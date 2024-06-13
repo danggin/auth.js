fa03e950f85ce39e6b2b30314add73737feb6583
Dain <danggin.min@gmail.com>
2024-06-13 17:00:11 +0900

    🚀 deploy: 배포를 위한 git 분기

---
+++ src/components/LoginForm.tsx
@@ -0,0 +1,20 @@
+import { githubLogin, login } from '@/lib/action';
+
+export default function LoginForm() {
+  return (
+    <>
+      <form action={login}>
+        <input type='text' name='email' placeholder='Enter Your Email' />
+        <input
+          type='password'
+          name='password'
+          placeholder='Enter Your Password'
+        />
+        <button>로그인</button>
+      </form>
+      <form action={githubLogin}>
+        <button>Github로 로그인</button>
+      </form>
+    </>
+  );
+}
